bf0f466192a67da18de8c41d0979ad21781fb1cf
celtic93 <egorka80@list.ru>
2022-05-16 21:47:57 +0300

    Added states for Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,30 @@ class Task < ApplicationRecord
 
   validates :name, :description, :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :to_develop do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :to_qa do
+      transition in_development: :in_qa
+    end
+
+    event :to_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :to_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+  end
 end
