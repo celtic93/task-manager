3184812e9dd9b938f68e9bc52cb3c9b5675cc377
celtic93 <egorka80@list.ru>
2022-05-17 21:40:47 +0300

    Updated email case sencitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true, format: { with: /@/ }
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /@/ }
   validates :first_name, :last_name, length: { minimum: 2 }
 end
